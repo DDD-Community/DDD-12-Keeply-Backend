d658fb43bf70cdf1488137bc389f76656477deac
JJhwan7099 <ghks7099@naver.com>
2025-07-11 18:53:20 +0900

    refactor: 토큰이 만료되거나 없을때 에러메시지 수정

--- src/main/kotlin/com/keeply/global/jwt/JwtFilter.kt
+++ src/main/kotlin/com/keeply/global/jwt/JwtFilter.kt
@@ -4,6 +4,7 @@ import jakarta.servlet.FilterChain
 import jakarta.servlet.ServletRequest
 import jakarta.servlet.ServletResponse
 import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.security.core.context.SecurityContextHolder
 import org.springframework.web.filter.GenericFilterBean
 import kotlin.text.startsWith
@@ -17,11 +18,25 @@ class JwtFilter(
         response: ServletResponse,
         filterChain: FilterChain
     ) {
+
         val jwtAccessToken = resolveToken(request as HttpServletRequest)
 
         if(jwtAccessToken != null && jwtProvider.validateToken(jwtAccessToken)) {
             val authentication = jwtProvider.getAuthentication(jwtAccessToken)
             SecurityContextHolder.getContext().authentication = authentication
+        } else {
+            var httpResponse = response as HttpServletResponse
+            httpResponse.status = 401
+            httpResponse.contentType = "application/json"
+            httpResponse.characterEncoding = "UTF-8"
+            httpResponse.writer.write("""
+                {
+                    "success": false,
+                    "message": "토큰이 없거나 유효하지 않습니다. 다시 로그인해주세요.",
+                    "data": null
+                }
+            """.trimIndent())
+            return
         }
 
         filterChain.doFilter(request, response)
