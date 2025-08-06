2f79d6b0d6b0c2130bd69492944ecabed29cd912
JJhwan7099 <ghks7099@naver.com>
2025-08-06 21:15:59 +0900

    feat: dependencies 수정

--- build.gradle.kts
+++ build.gradle.kts
@@ -31,7 +31,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-data-jpa")
-    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-web:3.5.0")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-oauth2-client")
